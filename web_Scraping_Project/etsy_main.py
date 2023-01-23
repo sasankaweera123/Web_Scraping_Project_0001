112bbeef69248df0510e2232108a5816277e78b2
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2023-01-23 16:06:54 +0530

    Update etsy_main.py
    Functionaries the Code

--- web_Scraping_Project/etsy_main.py
+++ web_Scraping_Project/etsy_main.py
@@ -2,29 +2,52 @@ from bs4 import BeautifulSoup
 import requests
 import openpyxl
 
-url = "https://www.etsy.com/listing/1328133422/custom-name-mirror-phone-case?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=iphone&ref=sc_gallery-1-1&pro=1&plkey=b0d11581c0f8bd5f1c53904a8c5a6bf151e8b971%3A1328133422"
 
-source = requests.get(url)
-source.raise_for_status()
-soup = BeautifulSoup(source.text, 'html.parser')
+def URL_format(url):
+    # get the source url
+    source = requests.get(url)
+    # validation about the url
+    source.raise_for_status()
+    # website url format using parser
+    soup = BeautifulSoup(source.text, 'html.parser')
 
-reviews = soup.find('div', {'id': 'reviews'})
+    return soup
 
-#wt-display-flex-xs
-count = reviews.find('div', {'class': 'wt-display-flex-xs'}).find('h2').text
-listOfChars = list()
 
-for char in count:
-    if char.isdigit():
-        listOfChars.append(char)
+def get_the_number_of_reviews(reviews):
 
-print(listOfChars)
+    #wt-display-flex-xs
+    count = reviews.find('div', {'class': 'wt-display-flex-xs'}).find('h2').text
+    listOfChars = list()
 
-print(count)
+    for char in count:
+        if char.isdigit():
+            listOfChars.append(char)
 
-items = reviews.find_all('div',class_="wt-text-body-01")
+    number_reviews=""
+    for i in range(len(listOfChars)):
+        number_reviews += listOfChars[i]
 
-# for item in items:
-#     print(item.find('p').text)
+    print(int(number_reviews))
 
-# print(items)
+    return int(number_reviews)
+
+def get_reviews(reviews):
+    items = reviews.find_all('div',class_="wt-text-body-01")
+    reviews_list = list()
+    for item in items:
+        review = item.find('p').text
+        review = review.replace('\n', '')
+        review=review.lstrip()
+        review = review.rstrip()
+        reviews_list.append(review)
+
+    print(reviews_list)
+    return reviews_list
+
+
+if __name__ == "__main__":
+    url = "https://www.etsy.com/listing/1328133422/custom-name-mirror-phone-case?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=iphone&ref=sc_gallery-1-1&pro=1&plkey=b0d11581c0f8bd5f1c53904a8c5a6bf151e8b971%3A1328133422"
+    reviews = URL_format(url).find('div', {'id': 'reviews'})
+    get_the_number_of_reviews(reviews)
+    get_reviews(reviews)
